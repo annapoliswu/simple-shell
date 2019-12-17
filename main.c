170dac81bcc417e5931a44647c7886021cb7e8b3
Anna Wu <annapoliswu@gmail.com>
2019-12-17 18:10:32 -0500

    init commit, simple shell

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "sh.h"
+#include <signal.h>
+#include <stdio.h>
+
+void sig_handler(int signal); 
+
+
+int main( int argc, char **argv, char **envp )
+{
+  /* put signal set up stuff here */
+  signal(SIGINT, sig_handler);
+  signal(SIGTSTP, sig_handler);
+  signal(SIGTERM, sig_handler);
+  signal(EOF, sig_handler);
+  return sh(argc, argv, envp);
+}
+
+void sig_handler(int signal)
+{
+  /* define your signal handler */
+  printf("\nIGNORED SIGNAL\n");
+
+}
